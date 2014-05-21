0c3f52574760da42a4b17ac7d0652efd09e83594
Greicy de Matias Silva <greicy_matias@hotmail.com>
2014-05-21 11:47:09 -0300

    cadastro

--- RepositorioCyberCode/Greicy/Program.cs
+++ RepositorioCyberCode/Greicy/Program.cs
@@ -8,11 +8,39 @@ namespace Greicy
 {
     class Program
     {
+        static int carroCadastrado = 0;
+        static int constante = 20;
+        static string[] Marca = new string[constante];
+        static string[] Placa = new string[constante];
+        static string[] Cor = new string[constante];
+        static double[] Quilometragem = new double[constante];
+        static string[] Nome = new string[constante];
+        static string[] RG = new string[constante];
+        static string[] CPF = new string[constante];        
+        static string[,] Endereco = new string[7,constante]; 
+        //Endereço: Linha 1 = Pais; Linha 2= Estado; Linha 3 =Cidade; Linha 4= Rua; Linha 5 = Bairro; Linha 6 = Numero; Linha 7 = Complemento
+        static void cadastroCarro()
+        {            
+            do{
+                Console.WriteLine("\t\tCadastrando um novo carro no sistema...\n\n");
+                Console.WriteLine("Marca: ");
+                Marca[carroCadastrado] = Console.ReadLine();
+                Console.WriteLine("Placa: \t(XXX-000)");
+                Placa[carroCadastrado] = Console.ReadLine();
+                Console.WriteLine("Cor: ");
+                Cor[carroCadastrado] = Console.ReadLine();
+                Console.WriteLine("Km Atual: ");
+                Quilometragem[carroCadastrado] = double.Parse(Console.ReadLine());
+                carroCadastrado +=1;
+                
+            }while();
+        }
+        static void cadastroCliente(){
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("PROGRAMANDO NO GITHUB BUGGADO");
-            Console.ReadKey();
-            Console.WriteLine("\a\a\a\a\a\a\a\a\a\a");
+            cadastroCarro();
         }
     }
 }
