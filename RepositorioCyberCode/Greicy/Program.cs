5d72dafc8fc62a203b86b992e6ef0da932432cbe
MateusC <mateus.martino@gmail.com>
2014-05-28 10:12:10 -0300

    .

--- RepositorioCyberCode/Greicy/Program.cs
+++ RepositorioCyberCode/Greicy/Program.cs
@@ -8,15 +8,17 @@ namespace Greicy
 {
     class Program
     {
-        static int carroCadastrado = 0;
-        static int clienteCadastrado = 0;
+        static int CarroCadastrado = 0;
+        static int ClienteCadastrado = 0;
         static int constante = 20;
-        static string opcao = null;
+        static string Opcao = null;
         static string Pesquisa = null;
         static int Achou = 0;
         static string Resposta = null;
         static string SenhaTeste = null;
         static int Posicao = 0;
+        static int Tentativas = 3;
+        static int CarrosRemovidos, ClientesRemovidos = 0;
 
         static string[] Marca = new string[constante];
         static string[] Placa = new string[constante];
@@ -37,69 +39,69 @@ namespace Greicy
         static void cadastro_Carro()
         {            
             do{
-                opcao = null;
+                Opcao = null;
                 Console.Clear();
                 Console.WriteLine("\t\tCadastrando um novo Carro \n\n");
                 Console.Write("Marca: ");
-                Marca[carroCadastrado] = Console.ReadLine();
+                Marca[CarroCadastrado] = Console.ReadLine();
                 do
                 {
                     Console.Write("Placa: \t(XXX000)");
-                    Placa[carroCadastrado] = Console.ReadLine();
-                } while (Placa[carroCadastrado].Length != 6);
+                    Placa[CarroCadastrado] = Console.ReadLine();
+                } while (Placa[CarroCadastrado].Length != 6);
                 Console.Write("Cor: ");
-                Cor[carroCadastrado] = Console.ReadLine();
+                Cor[CarroCadastrado] = Console.ReadLine();
                 Console.Write("Km Atual: ");
-                Quilometragem[carroCadastrado] = double.Parse(Console.ReadLine());
-                Situacao[carroCadastrado] = "Garagem";
-                DiasLocado[carroCadastrado] = 0;
+                Quilometragem[CarroCadastrado] = double.Parse(Console.ReadLine());
+                Situacao[CarroCadastrado] = "Garagem";
+                DiasLocado[CarroCadastrado] = 0;
                 Console.WriteLine("Qual o valor da diária deste carro ?");
-                ValorDiaria[carroCadastrado] = double.Parse(Console.ReadLine());
-                carroCadastrado +=1;
+                ValorDiaria[CarroCadastrado] = double.Parse(Console.ReadLine());
+                CarroCadastrado +=1;
                 Console.Write("\n\nDeseja cadastrar outro automóvel ?     (S/N) ");
-                opcao = Console.ReadLine();
-                opcao = opcao.ToLower();
+                Opcao = Console.ReadLine();
+                Opcao = Opcao.ToLower();
                 
-            }while( opcao != "n");
+            }while( Opcao != "n");
         }
         static void cadastro_Cliente(){
             do{
                 Console.Clear();
-                opcao = null;
+                Opcao = null;
                 Console.WriteLine("\t\t\t CADASTRO CLIENTE :\n\n");
                 Console.Write("Nome : ");
-                Nome[clienteCadastrado] = Console.ReadLine();
+                Nome[ClienteCadastrado] = Console.ReadLine();
                 do
                 {
                     Console.Write("RG : (10 digitos)");
-                    RG[clienteCadastrado] = Console.ReadLine();
-                } while (RG[clienteCadastrado].Length != 10 );
+                    RG[ClienteCadastrado] = Console.ReadLine();
+                } while (RG[ClienteCadastrado].Length != 10 );
                 do
                 {
                     Console.Write("CPF : (11 digitos)");
-                    CPF[clienteCadastrado] = Console.ReadLine();
-                } while (CPF[clienteCadastrado].Length != 11);
+                    CPF[ClienteCadastrado] = Console.ReadLine();
+                } while (CPF[ClienteCadastrado].Length != 11);
                 Console.Write("Pais : ");
-                Endereco[clienteCadastrado,0] = Console.ReadLine();
+                Endereco[ClienteCadastrado,0] = Console.ReadLine();
                 Console.Write("Estado : ");
-                Endereco[clienteCadastrado,1] = Console.ReadLine();
+                Endereco[ClienteCadastrado,1] = Console.ReadLine();
                 Console.Write("Cidade : ");
-                Endereco[clienteCadastrado,2] = Console.ReadLine();
+                Endereco[ClienteCadastrado,2] = Console.ReadLine();
                 Console.Write("Rua : ");
-                Endereco[clienteCadastrado,3] = Console.ReadLine();
+                Endereco[ClienteCadastrado,3] = Console.ReadLine();
                 Console.Write("Bairro : ");
-                Endereco[clienteCadastrado,4] = Console.ReadLine();
+                Endereco[ClienteCadastrado,4] = Console.ReadLine();
                 Console.Write("Numero : ");
-                Endereco[clienteCadastrado,5] = Console.ReadLine();
+                Endereco[ClienteCadastrado,5] = Console.ReadLine();
                 Console.Write("Complemento : ");
-                Endereco[clienteCadastrado,6] = Console.ReadLine();
-                Saldo[clienteCadastrado] = 0;
-                clienteCadastrado += 1;
+                Endereco[ClienteCadastrado,6] = Console.ReadLine();
+                Saldo[ClienteCadastrado] = 0;
+                ClienteCadastrado += 1;
                 Console.Write("\n\nDeseja cadastrar outro cliente ?     (S/N) ");
-                opcao = Console.ReadLine();
-                opcao = opcao.ToLower();
+                Opcao = Console.ReadLine();
+                Opcao = Opcao.ToLower();
                 
-            }while( opcao != "n");
+            }while( Opcao != "n");
         }
 
         static void Consulta_Cliente()
@@ -108,7 +110,7 @@ namespace Greicy
             {
                 Pesquisa = null;
                 Achou = 0;
-                opcao = null;
+                Opcao = null;
                 do
                 {
                     Console.Clear();
@@ -129,14 +131,15 @@ namespace Greicy
                         Console.WriteLine("\tRG...........: {0}", RG[i]);
                         Console.WriteLine("\tCPF..........: {0}", CPF[i]);
                         Console.WriteLine("\tPais.........: {0}", Endereco[i, 0]);
-                        Console.WriteLine("\tEstado.......: {0}", Endereco[clienteCadastrado, 1]);
-                        Console.WriteLine("\tCidade.......: {0}", Endereco[clienteCadastrado, 2]);
-                        Console.WriteLine("\tRua..........: {0}", Endereco[clienteCadastrado, 3]);
-                        Console.WriteLine("\tBairro.......: {0}", Endereco[clienteCadastrado, 4]);
-                        Console.WriteLine("\tNumero.......: {0}", Endereco[clienteCadastrado, 5]);
-                        Console.WriteLine("\tComplemento..: {0}", Endereco[clienteCadastrado, 6]);
+                        Console.WriteLine("\tEstado.......: {0}", Endereco[ClienteCadastrado, 1]);
+                        Console.WriteLine("\tCidade.......: {0}", Endereco[ClienteCadastrado, 2]);
+                        Console.WriteLine("\tRua..........: {0}", Endereco[ClienteCadastrado, 3]);
+                        Console.WriteLine("\tBairro.......: {0}", Endereco[ClienteCadastrado, 4]);
+                        Console.WriteLine("\tNumero.......: {0}", Endereco[ClienteCadastrado, 5]);
+                        Console.WriteLine("\tComplemento..: {0}", Endereco[ClienteCadastrado, 6]);
                         Console.WriteLine("\tSaldo........: {0}", Saldo[i]);
                         Achou = 1;
+                        Posicao = i;
                     }
                 }
                 if (Achou == 0)
@@ -144,10 +147,10 @@ namespace Greicy
                     Console.WriteLine("Cliente não cadastrado.");
                 }
                 Console.WriteLine("\n\nDeseja consultar outro cliente ?      (S/N)");
-                opcao = Console.ReadLine();
-                opcao = opcao.ToLower();
+                Opcao = Console.ReadLine();
+                Opcao = Opcao.ToLower();
 
-            } while (opcao != "n");
+            } while (Opcao != "n");
         }
 
         static void Consulta_Carros()
@@ -156,7 +159,7 @@ namespace Greicy
             do{               
                 Pesquisa = null;
                 Achou = 0;
-                opcao = null;
+                Opcao = null;
                 do {
                     Console.Clear();
                     Console.WriteLine("\t\t\t CONSULTA DE CARRO :\n\n");
@@ -169,13 +172,13 @@ namespace Greicy
                 }while(Pesquisa.Length != 6);
                 for(int i=0; i<constante;i++){
                     if (Pesquisa == Placa[i]){
-                        Console.WriteLine("Marca............: {0}",Marca[i]);
-                        Console.WriteLine("Placa............: {0}",Placa[i]);
-                        Console.WriteLine("Cor..............: {0}",Cor[i]);
-                        Console.WriteLine("Km Atual.........: {0}",Quilometragem[i]);
-                        Console.WriteLine("Situação.........: {0}",Situacao[i]);
-                        Console.WriteLine("Disponibilidade..: {0} dias",DiasLocado[i]);
-                        Console.WriteLine("Valor Diária.....: {0}", ValorDiaria[i]);
+                        Console.WriteLine("\tMarca............: {0}",Marca[i]);
+                        Console.WriteLine("\tPlaca............: {0}",Placa[i]);
+                        Console.WriteLine("\tCor..............: {0}",Cor[i]);
+                        Console.WriteLine("\tKm Atual.........: {0}",Quilometragem[i]);
+                        Console.WriteLine("\tSituação.........: {0}",Situacao[i]);
+                        Console.WriteLine("\tDisponibilidade..: {0} dias",DiasLocado[i]);
+                        Console.WriteLine("\tValor Diária.....: {0}", ValorDiaria[i]);
                         Achou = 1;
                         Posicao = i;
 
@@ -185,19 +188,19 @@ namespace Greicy
                     Console.WriteLine("Carro não cadastrado.");
                 }
                 Console.WriteLine("\n\nDeseja consultar outro carro ?      (S/N)");
-                opcao = Console.ReadLine();
-                opcao = opcao.ToLower();
+                Opcao = Console.ReadLine();
+                Opcao = Opcao.ToLower();
                 
-            }while( opcao != "n");               
+            }while( Opcao != "n");               
         }
 
         static void Tabela_Preco_Carros()
         {
             do{
-                opcao = null;
+                Opcao = null;
                 Console.Clear();
                 Console.WriteLine("\t\t\t TABELA DE PREÇOS \n\n");
-                for (int i = 0; i < carroCadastrado; i++)
+                for (int i = 0; i < CarroCadastrado; i++)
                 {
                     Console.WriteLine(" ");
                     Console.WriteLine("Marca............: {0}",Marca[i]);
@@ -206,53 +209,81 @@ namespace Greicy
                     Console.WriteLine("Valor Diária.....: {0}",ValorDiaria[i]);
                 }
                 Console.WriteLine("\n\nDeseja sair da tabela ?      (S/N)");
-                opcao = Console.ReadLine();
-                opcao = opcao.ToLower();
+                Opcao = Console.ReadLine();
+                Opcao = Opcao.ToLower();
                 
-            }while( opcao != "s");
+            }while( Opcao != "s");
         }
 
         static void Situacao_Dos_Carros()
         {
-            opcao = null;
-            Console.Clear();
             Console.WriteLine("\t\t\t SITUAÇÃO DOS CARROS \n\n");
-
             do{
-                for (int i = 0; i < carroCadastrado; i++)
+                Opcao = null;
+                Console.Clear();
+                for (int i = 0; i < CarroCadastrado; i++)
                 {
+                    Console.WriteLine(" ");
                     Console.WriteLine("Marca............: {0}", Marca[i]);
                     Console.WriteLine("Local............: {0}", Situacao[i]);
                     Console.WriteLine("Disponivel em....: {0} dias", DiasLocado[i]+1); 
                 }
-                Console.WriteLine("\n\nDeseja sair da consulta ?      (S/N)");
-                opcao = Console.ReadLine();
-                opcao = opcao.ToLower();
+                Console.WriteLine("\n\nDeseja sair ?      (S/N)");
+                Opcao = Console.ReadLine();
+                Opcao = Opcao.ToLower();
                 
-            }while( opcao != "s");
+            }while( Opcao != "s");
         }
 
         static void Relatorio_Cliente()
         {
-
+            Console.WriteLine("\t\t\t RELATORIO SOBRE CLIENTES :\n\n");
+            do{
+                Opcao = null;
+                Console.Clear();
+                for(int i=0;i<ClienteCadastrado;i++){
+                    Console.WriteLine("\nNome.........: {0}", Nome[i]);
+                    Console.WriteLine("RG...........: {0}", RG[i]);
+                    Console.WriteLine("CPF..........: {0}", CPF[i]);
+                    Console.WriteLine("Pais.........: {0}", Endereco[i, 0]);
+                    Console.WriteLine("Estado.......: {0}", Endereco[ClienteCadastrado, 1]);
+                    Console.WriteLine("Cidade.......: {0}", Endereco[ClienteCadastrado, 2]);
+                    Console.WriteLine("Rua..........: {0}", Endereco[ClienteCadastrado, 3]);
+                    Console.WriteLine("Bairro.......: {0}", Endereco[ClienteCadastrado, 4]);
+                    Console.WriteLine("Numero.......: {0}", Endereco[ClienteCadastrado, 5]);
+                    Console.WriteLine("Complemento..: {0}", Endereco[ClienteCadastrado, 6]);
+                    Console.WriteLine("Saldo........: {0}", Saldo[i]);
+                }
+                Console.WriteLine("\n\nDeseja sair da consulta ?      (S/N)");
+                Opcao = Console.ReadLine();
+                Opcao = Opcao.ToLower();
+            } while (Opcao != "s");
         }
 
         static void Relatorio_Carro()
         {
-
+            Console.WriteLine("\t\t\t RELATORIO SOBRE CARROS :\n\n");
+            do{
+                Opcao = null;
+                Console.Clear();
+                for (int i = 0; i < CarroCadastrado; i++)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Marca............: {0}", Marca[i]);
+                    Console.WriteLine("Placa............: {0}", Placa[i]);
+                    Console.WriteLine("Cor..............: {0}", Cor[i]);
+                    Console.WriteLine("Km Atual.........: {0}", Quilometragem[i]);
+                    Console.WriteLine("Situação.........: {0}", Situacao[i]);
+                    Console.WriteLine("Disponibilidade..: {0} dias", DiasLocado[i]);
+                    Console.WriteLine("Valor Diária.....: {0}", ValorDiaria[i]);
+                }
+                Console.WriteLine("\n\nDeseja sair da consulta ?      (S/N)");
+                Opcao = Console.ReadLine();
+                Opcao = Opcao.ToLower();
+            } while (Opcao != "s");
         }
 
-        static void Relatorio_Renda_Mensal()
-        {
 
-
-        }
-
-        static void Relatorio_Caixa()
-        {
-
-
-        }
 
 
         static void Main(string[] args)
